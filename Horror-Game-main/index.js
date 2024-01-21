4d912e1ecd8a0d42c03f2dc78ce231402011f39d
Jammas2me <156156758+Jammas2me@users.noreply.github.com>
2024-01-20 20:30:12 -0700

    Delete Horror-Game-main directory

--- Horror-Game-main/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-
-let server = app.listen(3000, () => {
-  console.log("Node server running...");
-});
-
-app.use(express.static("public"));
